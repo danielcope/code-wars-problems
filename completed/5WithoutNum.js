3c4a098ce08c46a29b4923168f71f7e3dec688e8
danielcope <daniel.ash.cope@gmail.com>
2022-02-28 21:39:51 -0700

    5 without numbbers problem

---
+++ completed/5WithoutNum.js
@@ -0,0 +1,13 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+//=================================================
+
+const unusualFive = () => {
+  let array = ["this", "is", "so", "weird", "right?"];
+
+  return array.length
+}
