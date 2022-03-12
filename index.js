94eae6154bd9cc85f124818b4f5f28d01ae95054
danielcope <daniel.ash.cope@gmail.com>
2022-03-12 12:21:43 -0700

    did some random raw js dom manipulation

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let animals = ["cat", "Chicken", "dog", "Pig", "Raccoon"];
-let returnValue = animals.filter((animal) => animal.includes("c"));
-console.log(returnValue);
+const buttonSectionEl = document.getElementById("button-container");
+
+for (let i = 1; i <= 20; i++) {
+  const buttonChild = document.createElement("button");
+  buttonChild.textContent = i;
+  buttonChild.onclick = () => alert(`You clicked button number ${i}`);
+  buttonSectionEl.appendChild(buttonChild);
+}
