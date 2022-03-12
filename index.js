86abe9a464d5e1efdb757b142cd077ca52f5355e
danielcope <daniel.ash.cope@gmail.com>
2022-03-12 16:23:22 -0700

    added style to buttons

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+// #region clickable buttons that alert the number
+
 const buttonSectionEl = document.getElementById("button-container");
 
 for (let i = 1; i <= 20; i++) {
+  const buttonContainerEl = document.createElement("section");
+
+  buttonContainerEl.className = "each-button";
+
   const buttonChild = document.createElement("button");
   buttonChild.textContent = i;
   buttonChild.onclick = () => alert(`You clicked button number ${i}`);
-  buttonSectionEl.appendChild(buttonChild);
+
+  buttonContainerEl.appendChild(buttonChild);
+  buttonSectionEl.appendChild(buttonContainerEl);
 }
+
+// #endregion clickable buttons that alert the number
