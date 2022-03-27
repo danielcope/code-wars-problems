9bff181a981a36687ee47c7b54fed2f54e97f9ee
danielcope <daniel.ash.cope@gmail.com>
2022-03-27 09:21:10 -0600

    added an emnumerate example to the cheet cheet

--- python/cheat_sheet.py
+++ python/cheat_sheet.py
@@ -1,9 +1,45 @@
-# 
+# -----------------------------------
+# defining a variable
+# different variables ("string", int, float, array, dictionary)
+rawr = "rawr"
+array = ['this', 'is', 'an', 'array']
+number = 5
+# like JS objects
+dictionary = {
+    "property": "value",
+}
 
-#   reversing a str or array
+
+# -----------------------------------
+# list comprehesion
+def list_comprehension(str):
+    result = [char for char in str]
+
+    return result
+
+# print(list_comprehension("rawr"))
+
+# ---------------------------------
+# reversing a str or array
+
+
 def reverse(str):
     return str[::-1]
 
 # print(reverse("something"))
 # print(reverse([1, 2, 3, 4, 5, 6]))
 
+# ---------------------------------------
+# for loop with enumerate()
+# this allows you to have a reference to the index of the element
+
+def for_enumerate(arr):
+    result = []
+
+    for index, ele in enumerate(arr, 0):
+        result.append([index, ele])
+
+    return result
+
+
+print(for_enumerate([1, 2, 3, 4, 5]))
